d96087d20e773b5ab958854ea0e75ea10a34176a
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2024-04-19 11:46:57 +0900

    FEAT 등록된 태그 조회

--- spring-be/src/main/java/flaskspring/demo/tag/repository/MemberTagLogRepository.java
+++ spring-be/src/main/java/flaskspring/demo/tag/repository/MemberTagLogRepository.java
@@ -1,7 +1,12 @@
 package flaskspring.demo.tag.repository;
 
+import flaskspring.demo.member.domain.Member;
 import flaskspring.demo.tag.domain.MemberTagLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberTagLogRepository extends JpaRepository<MemberTagLog, Long> {
+
+    List<MemberTagLog> findByMember(Member member);
 }
