b9fb00c0520171ef93a75fae19121e59b52baa8b
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2024-05-24 15:41:38 +0900

    FIX : API명세 수정

--- spring-be/src/main/java/flaskspring/demo/config/jwt/auth/AuthController.java
+++ spring-be/src/main/java/flaskspring/demo/config/jwt/auth/AuthController.java
@@ -40,7 +40,8 @@ public class AuthController {
     })
     @Operation(summary = "토큰 재발급", description = "Header " +
             "<br> Authorization : Bearer 만료토큰" +
-            "<br> Refresh-Token: Bearer 리프레시토큰")
+            "<br> Refresh-Token: Bearer 리프레시토큰" +
+            "<br> 재발급 요청 횟수 10회로 제한됨. 다시 로그인 한 경우 횟수 리셋")
     @PatchMapping("/reissue")
     public ResponseEntity<BaseResponse<ResReIssue>> reissue(HttpServletRequest request,
                                                         HttpServletResponse response,
