dd5ba0e3804b7e46b8834d3b36fcaac377cfa8c3
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2024-06-23 13:10:11 +0900

    FIX: 레디스 설정 변경

--- spring-be/src/main/java/flaskspring/demo/DemoApplication.java
+++ spring-be/src/main/java/flaskspring/demo/DemoApplication.java
@@ -1,12 +1,16 @@
 package flaskspring.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+		RedisAutoConfiguration.class})
 public class DemoApplication {
 
 	public static void main(String[] args) {
