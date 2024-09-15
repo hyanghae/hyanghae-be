a079a60469c1329510cb4e8e43e713b24e93e450
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2024-09-16 00:39:52 +0900

    FIX : 리스펀스 imgId 추가

--- spring-be/src/main/java/flaskspring/demo/member/dto/Res/ResUploadedImage.java
+++ spring-be/src/main/java/flaskspring/demo/member/dto/Res/ResUploadedImage.java
@@ -9,6 +9,9 @@ import java.time.LocalDateTime;
 @Data
 public class ResUploadedImage {
 
+    @Schema(description = "이미지 URL", example = "1")
+    private Long imgId;
+
     @Schema(description = "이미지 URL")
     private String imgUrl;
 
@@ -16,6 +19,7 @@ public class ResUploadedImage {
     private LocalDateTime uploadDate;
 
     public ResUploadedImage(UploadImage uploadImage) {
+        this.imgId = uploadImage.getId();
         this.imgUrl = uploadImage.getSavedImageUrl();
         this.uploadDate = uploadImage.getCreatedTime();
     }
