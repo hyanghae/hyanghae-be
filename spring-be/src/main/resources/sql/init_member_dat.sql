2b1335795067e007b4552562659969dbef7bdf29
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2024-10-01 21:36:23 +0900

    FIX : 데이터 변경

---
+++ spring-be/src/main/resources/sql/init_member_dat.sql
@@ -0,0 +1,6 @@
+INSERT INTO member (account,email,kakao_id,name,nickname,password,profile_image,role, is_onboarded, refresh_needed, recommend_possible, age_terms)
+VALUES ('testAccount1',NULL,NULL,'testName','유저-494218','$2a$10$xCecT9Re5hjUylZ8iDz4k.ny.pg6JKH3cMcfNrKpQSCUWV7yiAW3K',NULL,'USER', 0, 0, 0, 0);
+
+insert into member (account,email,kakao_id,name,nickname,password,profile_image,role) values ('testAccount2',NULL,NULL,'testName','유저-893407','$2a$10$d68SZJRkt8a.VJ2SLyoUTu/7czEdJNBFfAUeGOQne.OET7wgxnZ1O',NULL,'USER');
+insert into member (account,email,kakao_id,name,nickname,password,profile_image,role) values ('testAccount3',NULL,NULL,'testName','유저-957517','$2a$10$97utiYBNBOcGn.nmNN4EOuyCc1Yk2Y4yYL9r21uvPKFGZyajxhxJS',NULL,'USER');
+
